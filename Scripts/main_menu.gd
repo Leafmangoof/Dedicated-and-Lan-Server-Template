e5f70d3d95a2c6e972cd2f1b6c1d9f4f1485ccb8
Leafmangoof <evin8stamper@gmail.com>
2024-05-27 15:12:40 -0400

    updated_comments

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -19,11 +19,14 @@ func _on_join_button_down():
 
 func _on_option_button_item_selected(index):
 	if index == 0:
+		# "127.0.0.1" is your IP address to connect to your local network
+		# everyone has the same one
 		$VBoxContainer/address.text = "127.0.0.1"
 		$VBoxContainer/address.editable = false
 	elif index == 1:
 		$VBoxContainer/address.text = ""
 		$VBoxContainer/address.editable = true
+	
 	
 func _on_join_game_button_down():
 	Server.joinGame($VBoxContainer/address.text, $VBoxContainer/usernameEntry.text)
--- Scripts/server_scripts/GameManager.gd
+++ Scripts/server_scripts/GameManager.gd
@@ -2,4 +2,13 @@
 extends Node
 
 #This is for keeping track of players
+# You can imagine the data being stored like this
+#[codeblock]
+# ID :{
+#    "localGameHost": false
+#    "playerName": Example
+#    "id": ID
+#}
+#[/codeblock]
+# The ID is the key in the Players dictionary
 var Players = {}
